93aa2f480c6e70240b72bf3ffda347dc1ce660bd
Ekin Bacik <ekinbacik@gmail.com>
2021-12-02 19:52:33 +0300

    new code added

--- TkkalApp/Views/PeriodicalView.swift
+++ TkkalApp/Views/PeriodicalView.swift
@@ -10,25 +10,39 @@ import SwiftUI
 struct FirstPeriodical: View {
     var body: some View {
         ZStack{
-            NavigationView{
-            GeometryReader{ img in
+            TabView{
+                LinearGradient(gradient: Gradient(colors: [.red,.black]), startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea().overlay(PeriodicalImageView())
                 
-        Image("TestImage")
-            .resizable()
-            .scaledToFit()
-            .frame(width: img.size.width*1, height: img.size.height*0.2).offset(x:-100,y:20)
-            .background(RoundedRectangle(cornerRadius: 10)
-                            .frame(width: img.size.width*0.3, height: img.size.height*0.25).offset(x:-100,y:25)
-                            .foregroundColor(.white))
-              }
             }
         }
     }
 }
 
+struct PeriodicalImageView:View{
+    var body: some View{
+        GeometryReader{ geo in
+        Image("TestImage")
+                .resizable()
+                .clipped()
+                .frame(width:geo.size.width*0.4,height: geo.size.height*0.3)
+            ZStack{
+                Spacer()
+        Text("HAYAT COK IYI AFSDKJFS")
+                    .bold()
+                    .foregroundColor(.white)
+                .frame(width:540)
+                
+                Text("SKDFMSLDKFMLSDKFMSLDFKMSLDKFMSLDKFMLSDKFMSLDKFSDKLFMDKDLSFMSDLMFKSDLMFFKMSLDMFSLDFM")
+                    .font(.subheadline).offset(x:-50,y:70)
+                    .frame(width:120,height:100)
+            }
+        }
+    }
+}
 struct PeriodicalView_Previews: PreviewProvider {
     static var previews: some View {
         FirstPeriodical()
             .preferredColorScheme(.dark)
+        PeriodicalImageView()
     }
 }
